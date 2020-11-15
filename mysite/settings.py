81061eddbf1628e83b1f3d5cc23c5fa9660ae84f
pepe004 <akakpogladson63@gmail.com>
2020-11-15 23:34:53 +0100

    preparation du deploy1

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -108,3 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+MEDIA_URL='/medias/'
+
+MEDIA_ROOT =os.path.join(BASE_DIR,'medias')
+
+django_heroku.settings(locals())
